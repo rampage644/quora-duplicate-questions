1a32706ad32a276490a7716ab80c38fbe8ed6d78
Sergei Turukin <rampage644@gmail.com>
2017-04-06 14:36:21 +0300

    GridSearch, SGD classifier

--- train.py
+++ train.py
@@ -2,34 +2,46 @@
 
 from __future__ import (absolute_import, unicode_literals, print_function, division)
 
+import pickle
 import time
 import argparse
 import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss, accuracy_score
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 
 import models.simple
 import util.data
 import util.preprocess
 
+# TODO: refactor
+try:
+    with open('transformer.pckl', 'rb') as ifile:
+        transformer = pickle.load(ifile)
+except FileNotFoundError:
+    print('No transformer pickle, will train from scratch')
+    transformer = None
 
-def model_from(args):
-    return models.simple.PreprocessSimpleFeatires
-
-
-def data_from(path):
+def data_from(path, limit=None):
     # Data loading
     t0 = time.time()
-    data = util.data.load_data(path)
-    print(f'Data loading took {time.time() - t0:.3f} seconds')
+
+    data = pd.read_csv(path, nrows=limit)
+    print('Data loading took {:.3f} seconds'.format(time.time() - t0))
 
     # Data preprocessing
+    global transformer
     t0 = time.time()
-    data = util.preprocess.preprocess(data)
-    print(f'Data preprocessing took {time.time() - t0:.3f} seconds')
+    data, transformer = util.preprocess.preprocess_lsi(data, transformer)
+    if transformer:
+        with open('transformer.pckl', 'wb') as ofile:
+            pickle.dump(transformer, ofile)
+    print('Data preprocessing took {:.3f} seconds'.format(time.time() - t0))
 
     return data
 
@@ -38,19 +50,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--submit', action='store_true')
     parser.add_argument('--train_path', type=str, default='data/train.csv')
+    parser.add_argument('--limit', type=int, default=None)
     parser.add_argument('--test_path', type=str, default='data/test.csv')
     parser.add_argument('--output', type=str, default='submission.csv')
-    parser.add_argument('--model', type=str, default='simple')
+    parser.add_argument('--search', action='store_true')
     args = parser.parse_args()
 
-    train, val = train_test_split(data_from(args.train_path), train_size=0.8)
-    X, y = train[train.columns.drop(['is_duplicate', 'id', 'qid1', 'qid2'])], train.is_duplicate
-    Xval, yval = val[val.columns.drop(['is_duplicate', 'id', 'qid1', 'qid2'])], val.is_duplicate
+    train, val = train_test_split(data_from(args.train_path, args.limit), train_size=0.8)
+    # X, y = train[train.columns.drop(['is_duplicate', 'id', 'qid1', 'qid2'])], train.is_duplicate
+    # Xval, yval = val[val.columns.drop(['is_duplicate', 'id', 'qid1', 'qid2'])], val.is_duplicate
+    X, y = train[train.columns.drop('is_duplicate')], train.is_duplicate
+    Xval, yval = val[val.columns.drop('is_duplicate')], val.is_duplicate
 
-    model = RandomForestClassifier(n_jobs=4)
-    model.fit(X, y)
-
-    def results(X, y, prefix):
+    def results(model, X, y, prefix):
         predicted = model.predict_proba(X)
         score = model.score(X, y)
         loss = log_loss(y, predicted)
@@ -58,18 +70,86 @@ def main():
             accuracy = accuracy_score(y, predicted)
         except ValueError:
             accuracy = 0.0
-        print(f'{prefix} score: {score:.2f}, loss: {loss:.4f}, accuracy: {accuracy:.2f}')
+        print('{} score: {:.2f}, loss: {:.4f}, accuracy: {:.2f}'.format(
+            prefix, score, loss, accuracy
+        ))
 
-    results(X, y, 'Training')
-    results(Xval, yval, 'Validation')
+    model = SGDClassifier(loss='log', n_iter=1, n_jobs=-1, alpha=0.001)
+
+    t0 = time.time()
+    if args.search:
+        parameters = {
+            # 'penalty': ['l1', 'l2'],
+            # 'C': [1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3]
+            # 'n_estimators': [50, 100, 200, 300, 500],
+            'loss': ['log', 'modified_huber'],
+            'penalty': ['l2', 'l1', 'elasticnet'],
+            'alpha': np.logspace(-6, 6, 13),
+            'n_iter': [1, 3, 5, 10]
+        }
+
+        grid_search = GridSearchCV(model, parameters, n_jobs=-1, verbose=1)
+
+        print('Performing grid search...')
+        grid_search.fit(X, y)
+        print('done in {:.3f}s'.format(time.time() - t0))
+        print()
+
+        print('Best score: {:.3f}'.format(grid_search.best_score_))
+        print('Best parameters set:')
+        best_parameters = grid_search.best_estimator_.get_params()
+        for param_name in sorted(parameters.keys()):
+            print('\t%s: %r' % (param_name, best_parameters[param_name]))
+    else:
+        model.fit(X, y)
+        print('Model training took {:.3f} seconds'.format(time.time() - t0))
+        results(model, X, y, 'Training')
+        results(model, Xval, yval, 'Validation')
+
+
 
     if args.submit:
         print('Submitting..')
-        test = data_from(args.test_path)
-        X = test[test.columns.drop(['test_id'])]
-        test['is_duplicate'] = model.predict_proba(X)[:, 1]
-        test.to_csv(args.output, index=False, header=True, columns=['test_id', 'is_duplicate'])
+        X = data_from(args.test_path, args.limit)
+        X['is_duplicate'] = model.predict_proba(X)[:, 1]
+        X['test_id'] = np.arange(len(X))
+        X.to_csv(args.output, index=False, header=True, columns=['test_id', 'is_duplicate'])
 
 
 if __name__ == '__main__':
     main()
+
+
+#%%
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from nltk.stem.porter import PorterStemmer
+# from nltk.corpus import stopwords
+# from nltk.stem import WordNetLemmatizer
+
+# import numpy as np
+# import pandas as pd
+# import nltk
+# import itertools
+# import util.data
+# import string
+
+# data = util.data.load_data('data/train.csv')
+# data.question1 = data.question1.apply(str)
+# data.question2 = data.question2.apply(str)
+
+# stops = set(stopwords.words('english'))
+# s = PorterStemmer()
+# l = WordNetLemmatizer()
+# def tokenize(text):
+#     text = text.lower()
+#     return [l.lemmatize(t) for t in nltk.word_tokenize(text) if t not in stops and t not in string.punctuation]
+
+# vectorizer = TfidfVectorizer(min_df=1, tokenizer=tokenize)
+# g = itertools.chain(data.question1, data.question2)
+
+# vectorizer.fit(g)
+
+# vectorizer.vocabulary_
+
+# nltk.word_tokenize('askjdb asdbasiuy asjihdjhask asdbas')
+# s.stem('oed')
--- util/preprocess.py
+++ util/preprocess.py
@@ -3,6 +3,24 @@ from __future__ import (absolute_import, unicode_literals, print_function, divis
 
 import string
 import spacy
+import string
+import nltk
+
+import numpy as np
+import pandas as pd
+import itertools
+
+from numpy.linalg import norm
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from Stemmer import Stemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def common_words(x):
@@ -30,8 +48,14 @@ def preprocess(X):
         x = str(x).lower()
         return x.translate(translator)
 
-    X.question1 = X.question1.apply(clean)
-    X.question2 = X.question2.apply(clean)
+
+    l = WordNetLemmatizer()
+    def clean_nltk(x):
+        x = str(x).lower()
+        return ' '.join([l.lemmatize(t) for t in nltk.word_tokenize(x) if t not in stops and t not in string.punctuation])
+
+    X.question1 = X.question1.apply(clean_simple)
+    X.question2 = X.question2.apply(clean_simple)
 
     X['q1_words_num'] = X['question1'].map(words_count)
     X['q2_words_num'] = X['question2'].map(words_count)
@@ -45,3 +69,43 @@ def preprocess(X):
     del X['question2']
 
     return X
+
+
+tr = str.maketrans('', '', string.punctuation)
+l = WordNetLemmatizer()
+s = Stemmer('english')
+def tokenize(text):
+    return s.stemWords(nltk.word_tokenize(text.translate(tr)))
+
+
+def preprocess_lsi(X, transformer):
+    X.question1 = X.question1.apply(str)
+    X.question2 = X.question2.apply(str)
+
+    vectorizer = TfidfVectorizer(min_df=1, stop_words='english', tokenizer=tokenize)
+    svd_model = TruncatedSVD(n_components=500, algorithm='randomized', n_iter=5)
+    svd_transformer = Pipeline([('tfidf', vectorizer),
+                                ('svd', svd_model)])
+
+    g = itertools.chain(X.question1, X.question2)
+    if not transformer:
+        svd_transformer.fit(g)
+        transformer = svd_transformer
+
+    x1, x2 = transformer.transform(X.question1), transformer.transform(X.question2)
+    # Memory inefficient
+    # X['cosine_distance'] = np.diag(cosine_similarity(x1, x2))
+    cosine = lambda x, y: x.T.dot(y) / norm(x) / norm(y)
+    X['cosine_distance'] = np.array([cosine(x, y) if x.sum() and y.sum() else 0.0 for x, y in zip(x1, x2)])
+    X['l2_distance'] = np.linalg.norm(x1 - x2, axis=1)
+    X['l1_distance'] = np.abs(x1 - x2).sum(axis=1)
+
+    del X['question1']
+    del X['question2']
+    ret = pd.DataFrame(x1-x2)
+    try:
+        ret['is_duplicate'] = X.is_duplicate
+    except AttributeError:
+        pass
+
+    return ret, transformer
