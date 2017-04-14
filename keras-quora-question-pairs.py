9c57e56ca824ed0c1f63fb1772e6470859fbc188
Sergei Turukin <rampage644@gmail.com>
2017-04-14 10:03:34 +0300

    Internet solutions modified

--- keras-quora-question-pairs.py
+++ keras-quora-question-pairs.py
@@ -6,7 +6,8 @@ from os.path import expanduser, exists
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Embedding, Dense, Dropout, Reshape, Merge, BatchNormalization, TimeDistributed, Lambda
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers import Embedding, Dense, Dropout, Reshape, Merge, BatchNormalization, TimeDistributed, Lambda, Convolution1D, GlobalMaxPooling1D
 from keras.regularizers import l2
 from keras.callbacks import Callback, ModelCheckpoint
 from keras.utils.data_utils import get_file
@@ -16,24 +17,36 @@ from sklearn.model_selection import train_test_split
 KERAS_DATASETS_DIR = expanduser('~/.keras/datasets/')
 QUESTION_PAIRS_FILE_URL = 'http://qim.ec.quoracdn.net/quora_duplicate_questions.tsv'
 QUESTION_PAIRS_FILE = 'quora_duplicate_questions.tsv'
+QUESTION_PAIRS_TEST_FILE = 'data/test.csv'
 GLOVE_ZIP_FILE_URL = 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
 GLOVE_ZIP_FILE = 'glove.840B.300d.zip'
 GLOVE_FILE = 'glove.840B.300d.txt'
 Q1_TRAINING_DATA_FILE = 'q1_train.npy'
 Q2_TRAINING_DATA_FILE = 'q2_train.npy'
+Q1_TEST_DATA_FILE = 'q1_test.npy'
+Q2_TEST_DATA_FILE = 'q2_test.npy'
 LABEL_TRAINING_DATA_FILE = 'label_train.npy'
 WORD_EMBEDDING_MATRIX_FILE = 'word_embedding_matrix.npy'
 NB_WORDS_DATA_FILE = 'nb_words.json'
 MAX_NB_WORDS = 200000
-MAX_SEQUENCE_LENGTH = 25
+MAX_SEQUENCE_LENGTH = 40
 EMBEDDING_DIM = 300
 MODEL_WEIGHTS_FILE = 'question_pairs_weights.h5'
 VALIDATION_SPLIT = 0.1
 TEST_SPLIT = 0.1
 RNG_SEED = 13371447
-NB_EPOCHS = 25
+NB_EPOCHS = 50
+DO_RATE = 0.35
+nb_filter = 64
+filter_length = 5
 
-if exists(Q1_TRAINING_DATA_FILE) and exists(Q2_TRAINING_DATA_FILE) and exists(LABEL_TRAINING_DATA_FILE) and exists(NB_WORDS_DATA_FILE) and exists(WORD_EMBEDDING_MATRIX_FILE):
+if (exists(Q1_TRAINING_DATA_FILE) and
+    exists(Q2_TRAINING_DATA_FILE) and
+    exists(LABEL_TRAINING_DATA_FILE) and
+    exists(NB_WORDS_DATA_FILE) and
+    exists(WORD_EMBEDDING_MATRIX_FILE) and
+    exists(Q1_TEST_DATA_FILE) and
+    exists(Q2_TEST_DATA_FILE)):
     q1_data = np.load(open(Q1_TRAINING_DATA_FILE, 'rb'))
     q2_data = np.load(open(Q2_TRAINING_DATA_FILE, 'rb'))
     labels = np.load(open(LABEL_TRAINING_DATA_FILE, 'rb'))
@@ -52,17 +65,26 @@ else:
     with open(KERAS_DATASETS_DIR + QUESTION_PAIRS_FILE, encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
         for row in reader:
-            question1.append(row['text1'])
-            question2.append(row['text2'])
-            is_duplicate.append(row['duplicate'])
+            question1.append(row['question1'])
+            question2.append(row['question2'])
+            is_duplicate.append(row['is_duplicate'])
+
+    test_question1 = []
+    test_question2 = []
+    with open(QUESTION_PAIRS_TEST_FILE, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            test_question1.append(row['question1'])
+            test_question2.append(row['question2'])
 
     print('Question pairs: %d' % len(question1))
 
-    questions = question1 + question2
     tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
-    tokenizer.fit_on_texts(questions)
+    tokenizer.fit_on_texts(question1 + question2 + test_question1 + test_question2)
     question1_word_sequences = tokenizer.texts_to_sequences(question1)
     question2_word_sequences = tokenizer.texts_to_sequences(question2)
+    t_question1_word_sequences = tokenizer.texts_to_sequences(test_question1)
+    t_question2_word_sequences = tokenizer.texts_to_sequences(test_question2)
     word_index = tokenizer.word_index
 
     print("Words in index: %d" % len(word_index))
@@ -91,18 +113,24 @@ else:
         embedding_vector = embeddings_index.get(word)
         if embedding_vector is not None:
             word_embedding_matrix[i] = embedding_vector
-        
+
     print('Null word embeddings: %d' % np.sum(np.sum(word_embedding_matrix, axis=1) == 0))
 
     q1_data = pad_sequences(question1_word_sequences, maxlen=MAX_SEQUENCE_LENGTH)
     q2_data = pad_sequences(question2_word_sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    t_q1_data = pad_sequences(t_question1_word_sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    t_q2_data = pad_sequences(t_question2_word_sequences, maxlen=MAX_SEQUENCE_LENGTH)
     labels = np.array(is_duplicate, dtype=int)
     print('Shape of question1 data tensor:', q1_data.shape)
     print('Shape of question2 data tensor:', q2_data.shape)
+    print('Shape of t_question1 data tensor:', t_q1_data.shape)
+    print('Shape of t_question2 data tensor:', t_q2_data.shape)
     print('Shape of label tensor:', labels.shape)
 
     np.save(open(Q1_TRAINING_DATA_FILE, 'wb'), q1_data)
     np.save(open(Q2_TRAINING_DATA_FILE, 'wb'), q2_data)
+    np.save(open(Q1_TEST_DATA_FILE, 'wb'), t_q1_data)
+    np.save(open(Q2_TEST_DATA_FILE, 'wb'), t_q2_data)
     np.save(open(LABEL_TRAINING_DATA_FILE, 'wb'), labels)
     np.save(open(WORD_EMBEDDING_MATRIX_FILE, 'wb'), word_embedding_matrix)
     with open(NB_WORDS_DATA_FILE, 'w') as f:
@@ -119,38 +147,102 @@ Q2_test = X_test[:,1]
 Q1 = Sequential()
 Q1.add(Embedding(nb_words + 1, EMBEDDING_DIM, weights=[word_embedding_matrix], input_length=MAX_SEQUENCE_LENGTH, trainable=False))
 Q1.add(TimeDistributed(Dense(EMBEDDING_DIM, activation='relu')))
-Q1.add(Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM, )))
+Q1.add(Lambda(lambda x: K.sum(x, axis=1), output_shape=(EMBEDDING_DIM, )))
 Q2 = Sequential()
 Q2.add(Embedding(nb_words + 1, EMBEDDING_DIM, weights=[word_embedding_matrix], input_length=MAX_SEQUENCE_LENGTH, trainable=False))
 Q2.add(TimeDistributed(Dense(EMBEDDING_DIM, activation='relu')))
-Q2.add(Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM, )))
+Q2.add(Lambda(lambda x: K.sum(x, axis=1), output_shape=(EMBEDDING_DIM, )))
+
+
+model3 = Sequential()
+model3.add(Embedding(nb_words + 1,
+                     EMBEDDING_DIM,
+                     weights=[word_embedding_matrix],
+                     input_length=MAX_SEQUENCE_LENGTH,
+                     trainable=False))
+model3.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+model3.add(Dropout(DO_RATE))
+
+model3.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+
+model3.add(GlobalMaxPooling1D())
+model3.add(Dropout(DO_RATE))
+
+model3.add(Dense(EMBEDDING_DIM))
+model3.add(Dropout(DO_RATE))
+model3.add(BatchNormalization())
+
+model4 = Sequential()
+model4.add(Embedding(nb_words + 1,
+                     EMBEDDING_DIM,
+                     weights=[word_embedding_matrix],
+                     input_length=MAX_SEQUENCE_LENGTH,
+                     trainable=False))
+model4.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+model4.add(Dropout(DO_RATE))
+
+model4.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+
+model4.add(GlobalMaxPooling1D())
+model4.add(Dropout(DO_RATE))
+
+model4.add(Dense(EMBEDDING_DIM))
+model4.add(Dropout(DO_RATE))
+model4.add(BatchNormalization())
+
 model = Sequential()
-model.add(Merge([Q1, Q2], mode='concat'))
+model.add(Merge([Q1, Q2, model3, model4], mode='concat'))
 model.add(BatchNormalization())
-model.add(Dense(200, activation='relu'))
+model.add(Dense(EMBEDDING_DIM, activation='relu'))
+model.add(Dropout(DO_RATE))
 model.add(BatchNormalization())
-model.add(Dense(200, activation='relu'))
+model.add(Dense(EMBEDDING_DIM, activation='relu'))
+model.add(Dropout(DO_RATE))
 model.add(BatchNormalization())
-model.add(Dense(200, activation='relu'))
+model.add(Dense(EMBEDDING_DIM, activation='relu'))
+model.add(Dropout(DO_RATE))
 model.add(BatchNormalization())
-model.add(Dense(200, activation='relu'))
+model.add(Dense(EMBEDDING_DIM, activation='relu'))
+model.add(Dropout(DO_RATE))
+model.add(BatchNormalization())
+model.add(Dense(EMBEDDING_DIM, activation='relu'))
+model.add(Dropout(DO_RATE))
+model.add(BatchNormalization())
+model.add(Dense(EMBEDDING_DIM, activation='relu'))
+model.add(Dropout(DO_RATE))
 model.add(BatchNormalization())
 model.add(Dense(1, activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy', 
-              optimizer='adam', 
-              metrics=['accuracy', 'precision', 'recall', 'fbeta_score'])
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
 callbacks = [ModelCheckpoint(MODEL_WEIGHTS_FILE, monitor='val_acc', save_best_only=True)]
 
 print("Starting training at", datetime.datetime.now())
 
 t0 = time.time()
-history = model.fit([Q1_train, Q2_train], 
-                    y_train, 
-                    nb_epoch=NB_EPOCHS, 
-                    validation_split=VALIDATION_SPLIT, 
-                    verbose=1, 
+history = model.fit([Q1_train, Q2_train, Q1_train, Q2_train],
+                    y_train,
+                    nb_epoch=NB_EPOCHS,
+                    validation_split=VALIDATION_SPLIT,
+                    verbose=1,
                     callbacks=callbacks)
 t1 = time.time()
 
@@ -160,10 +252,7 @@ print("Minutes elapsed: %f" % ((t1 - t0) / 60.))
 
 model.load_weights(MODEL_WEIGHTS_FILE)
 
-loss, accuracy, precision, recall, fbeta_score = model.evaluate([Q1_test, Q2_test], y_test)
+loss, accuracy = model.evaluate([Q1_test, Q2_test, Q1_test, Q2_test], y_test)
 print('')
 print('loss      = {0:.4f}'.format(loss))
 print('accuracy  = {0:.4f}'.format(accuracy))
-print('precision = {0:.4f}'.format(precision))
-print('recall    = {0:.4f}'.format(recall))
-print('F         = {0:.4f}'.format(fbeta_score))
